77b247e4969d5d5aeaa59d0b6f4f1c1f8d180b59
dovilepalavinskaite <palavinskaite.dovile@gmail.com>
2021-04-02 17:20:30 +0300

    Navigation added. JS codes for menu open and close

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+// Content delay, appears while scrolling:
+
 const boxes = document.querySelectorAll('.box');
 
 window.addEventListener('scroll', checkBoxes);
@@ -16,4 +19,38 @@ function checkBoxes() {
 			box.classList.remove('show');
 		}
 	})
+}
+
+// Navbar menu on/of
+
+const header = document.querySelector("header");
+const body = document.querySelector("body");
+const navBurger = document.querySelectorAll(".nav-burger");
+const html = document.querySelector("html");
+const menu = document.querySelector(".nav-menu");
+
+navBurger.forEach(item => {
+	item.addEventListener('click', function() {
+		item.classList.toggle("opened");
+		if (item.classList.contains("opened")){
+			menu.classList.add("display");
+			stopScroll();
+		} else {
+			menu.classList.remove("display");
+			allowScroll();
+		}
+	});
+});
+
+
+// Freeze scrolling:
+
+function stopScroll() {
+	body.style.overflow = 'hidden';
+}
+
+// Let scrolling: 
+
+function allowScroll() {
+	body.style.overflow = 'visible';
 }
